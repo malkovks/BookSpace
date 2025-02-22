394407462195a3356cef535d807dc139651828be
Konstantin Malkov <malkovks.job@gmail.com>
2025-02-22 10:43:45 +0300

    Initial Commit

---
+++ BookSpace/BookSpaceApp.swift
@@ -0,0 +1,33 @@
+//
+// File name: BookSpaceApp.swift
+// Package: BookSpace
+//
+// Created by Malkov Konstantin on 22.02.2025.
+// Copyright (c) 2024 Malkov Konstantin . All rights reserved.
+
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BookSpaceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
